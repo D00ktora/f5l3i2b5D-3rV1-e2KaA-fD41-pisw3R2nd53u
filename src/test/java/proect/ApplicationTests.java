599f60c041d09350b73f165597ba8742cf345185
Stilyan Petrov <f.and.g.2019@gmail.com>
2024-02-20 18:33:14 +0200

    Project
    Commit the project

---
+++ src/test/java/proect/ApplicationTests.java
@@ -0,0 +1,13 @@
+package proect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
